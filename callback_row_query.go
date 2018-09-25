e756cbf80f5feda5277ad8d595acb381ab16e643
abba.hjy <abba.hjy@gmail.com>
2018-09-25 20:22:55 +0800

    alet support  setting

--- callback_row_query.go
+++ callback_row_query.go
@@ -1,6 +1,9 @@
 package gorm
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 // Define callbacks for row query
 func init() {
@@ -20,7 +23,9 @@ type RowsQueryResult struct {
 func rowQueryCallback(scope *Scope) {
 	if result, ok := scope.InstanceGet("row_query_result"); ok {
 		scope.prepareQuerySQL()
-
+		if str, ok := scope.Get("gorm:query_front_option"); ok {
+			scope.SQL = fmt.Sprint(str) + scope.SQL
+		}
 		if rowResult, ok := result.(*RowQueryResult); ok {
 			rowResult.Row = scope.SQLDB().QueryRow(scope.SQL, scope.SQLVars...)
 		} else if rowsResult, ok := result.(*RowsQueryResult); ok {
